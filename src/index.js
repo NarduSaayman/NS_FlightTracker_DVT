417b28cf1e29e6a16511680b69e532fe0de2a703
NarduSaayman <nsaayman@jhb.dvt.co.za>
2022-02-09 09:57:04 +0200

    feat: :sparkles: Added leaflet, Implemented leaflet quickstart, created config.js for snowpack

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+
+//Leaflet setup
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+//Get API calls from openskynetwork 
+
