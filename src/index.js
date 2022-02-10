3ff8ee2cf7b718578561da8f9db20fe75d6d7863
NarduSaayman <nsaayman@jhb.dvt.co.za>
2022-02-10 11:19:55 +0200

    refactor: :recycle: Added plane-solid icon as map marker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import L from "leaflet";
+import L, { Draggable } from "leaflet";
 import "leaflet/dist/leaflet.css";
 
+var PlaneSolid = L.icon({
+  iconUrl:"./assets/plane-solid.png",
+  iconSize: [50,50]
+});
 
 //Leaflet setup
 var map = L.map('map').setView([51.505, -0.09], 13);
@@ -9,9 +13,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+L.marker([51.5, -0.09], {icon: PlaneSolid}).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
+
 
 //Get API calls from openskynetwork 
 
