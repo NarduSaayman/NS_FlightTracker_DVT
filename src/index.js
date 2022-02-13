434d5c2045752626d5b138ab11eb4e6c338b859f
NarduSaayman <nsaayman@jhb.dvt.co.za>
2022-02-13 18:25:37 +0200

    refactor: :recycle: Adjusted amount of flights being tracked

--- src/index.js
+++ src/index.js
@@ -29,7 +29,8 @@ fetch('https://opensky-network.org/api/states/all')
             10 = true_track
     */
    
-    for (let i = 0; i < 200; i++) {
+    //Plot flights
+    for (let i = 0; i < 300; i++) {
         var flight = flights[i];
         var callsign = flight[1]; // callsign
         var origin_country = flight[2]; // origin_country
@@ -44,18 +45,15 @@ fetch('https://opensky-network.org/api/states/all')
         L.marker([latitude, longitude], 
             {icon: L.divIcon({
                 className: "leaflet-plane-marker",
-                html: L.Util.template('<svg style="filter: invert(57%) sepia(11%) saturate(3678%) hue-rotate(189deg) brightness(98%) contrast(98%); -webkit-transform: rotate('+ true_track +'deg); -moz-transform:rotate('+ true_track +'deg);" version="1.0" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 97.239 100" enable-background="new 0 0 97.239 100" xml:space="preserve"><path d="M42.732,5.521c0.01-7.396,11.138-7.396,11.151,0.207v30.956l43.355,26.108v11.412L54.091,60.032v23.137l9.953,7.814V100l-15.373-4.741L33.248,100v-9.017l9.9-7.814V60.032L0,74.205V62.792l42.732-26.108V5.521L42.732,5.521z"></path></svg>', iconSettings), //.replace('#','%23'),
+                html: L.Util.template('<svg style="filter: invert(57%) sepia(11%) saturate(3678%) hue-rotate(189deg) brightness(98%) contrast(98%); -webkit-transform: rotate('+ true_track +'deg); -moz-transform:rotate('+ true_track +'deg);" version="1.0" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 97.239 100" enable-background="new 0 0 97.239 100" xml:space="preserve"><path d="M42.732,5.521c0.01-7.396,11.138-7.396,11.151,0.207v30.956l43.355,26.108v11.412L54.091,60.032v23.137l9.953,7.814V100l-15.373-4.741L33.248,100v-9.017l9.9-7.814V60.032L0,74.205V62.792l42.732-26.108V5.521L42.732,5.521z"></path></svg>'),
                 iconAnchor  : [12, 32],
                 iconSize    : [25, 30],
                 popupAnchor : [0, -28]
-            
             })
         
         }).addTo(map)
         .bindPopup('Flight: ' + callsign + ' - From: ' + origin_country)
         }
-
     }
-
 })
 .catch((err) => console.log(err));
