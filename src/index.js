635de6e713aa8d234a7e4982dd70679941835bf6
NarduSaayman <nsaayman@jhb.dvt.co.za>
2022-02-14 14:38:37 +0200

    fix: :bug: Fixed eventHandler for flight items, thank you Tim!

--- src/index.js
+++ src/index.js
@@ -14,13 +14,17 @@ maxZoom: 22
 
 //Get API calls from openskynetwork
 var data;
-var flights;
+var states;
+
+var flights = [];
+
 
 fetch('https://opensky-network.org/api/states/all')
-.then((response) => response.json())
+.then((response) => response?.json())
 .then((json) => {
     data = json;
-    flights = data.states;
+    states = data?.states?.filter(state => !!state[0] && state[8] != false && !!state[5] && !!state[6] && !!state[1]).slice(0,200);
+
     /*
     State - 0 = icao24address
             1 = callsign
@@ -31,51 +35,77 @@ fetch('https://opensky-network.org/api/states/all')
             10 = true_track
     */
    
-    //Plot flights
-    for (let i = 0; i < 300; i++) {
-        var flight = flights[i];
-        var icao24address = flight[0]; // icao24bit
-        var callsign = flight[1]; // callsign
-        var origin_country = flight[2]; // origin_country
-        var longitude = flight[5]; // longitude
-        var latitude = flight[6]; // latitude
-        var on_ground = flight[8]; // on_ground
-        var true_track = flight[10]; // true_track - plane direction
+    // //Plot flights
+    // for (let i = 0; i < 100; i++) {
+    //     var state = states[i];
 
-        //If flight is airborne and has coords
-        if (flight != null 
-            && icao24address != null
-            && on_ground == false 
-            && longitude != null
-            && latitude != null
-            && callsign != null) {
+    states.forEach(state => {
+        
+        const flight = {
+            icao24address: state[0],
+            callsign: state[1],
+            origin_country: state[2],
+            longitude: state[5],
+            latitude: state[6],
+            on_ground: state[8],
+            true_track: state[10]
+        };
+
+        // //If flight is airborne and has coords
+        // if (state != null 
+        //     && flight.icao24address != null
+        //     && flight.on_ground == false 
+        //     && flight.longitude != null
+        //     && flight.latitude != null
+        //     && flight.callsign != null) {
+
             
-        L.marker([latitude, longitude], 
+        //push onto flights array
+            
+            flights.push(flight);
+        
+        //Setup flight marker
+        L.marker([flight.latitude, flight.longitude], 
             {icon: L.divIcon({
                 className: "leaflet-plane-marker",
-                html: L.Util.template('<svg style="filter: invert(57%) sepia(11%) saturate(3678%) hue-rotate(189deg) brightness(98%) contrast(98%); -webkit-transform: rotate('+ true_track +'deg); -moz-transform:rotate('+ true_track +'deg);" version="1.0" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 97.239 100" enable-background="new 0 0 97.239 100" xml:space="preserve"><path d="M42.732,5.521c0.01-7.396,11.138-7.396,11.151,0.207v30.956l43.355,26.108v11.412L54.091,60.032v23.137l9.953,7.814V100l-15.373-4.741L33.248,100v-9.017l9.9-7.814V60.032L0,74.205V62.792l42.732-26.108V5.521L42.732,5.521z"></path></svg>'),
+                html: L.Util.template('<svg style="filter: invert(57%) sepia(11%) saturate(3678%) hue-rotate(189deg) brightness(98%) contrast(98%); -webkit-transform: rotate('+ flight.true_track +'deg); -moz-transform:rotate('+ flight.true_track +'deg);" version="1.0" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 97.239 100" enable-background="new 0 0 97.239 100" xml:space="preserve"><path d="M42.732,5.521c0.01-7.396,11.138-7.396,11.151,0.207v30.956l43.355,26.108v11.412L54.091,60.032v23.137l9.953,7.814V100l-15.373-4.741L33.248,100v-9.017l9.9-7.814V60.032L0,74.205V62.792l42.732-26.108V5.521L42.732,5.521z"></path></svg>'),
                 iconAnchor  : [12, 32],
                 iconSize    : [25, 30],
                 popupAnchor : [0, -28]
             })
         
         }).addTo(map)
-        .bindPopup('Flight: ' + callsign + ' - From: ' + origin_country)
-        }
+        .bindPopup('Flight: ' + flight.callsign + ' - From: ' + flight.origin_country);
 
-        // add a flight element to flights conatiner
+
+        // add flight item to flights conatiner
         var flightItem = document.createElement("div");
         flightItem.className = "flight-item";
-        flightItem.id = icao24address;
-        flightItem.innerHTML = "<p>Flight - " + callsign + "</p><p>Origin - " + origin_country + "</p>" ;
+        flightItem.id = flight.icao24address;
+        flightItem.innerHTML = "<p>Flight - " + flight.callsign + "</p><p>Origin - " + flight.origin_country + "</p>" ;
+        
+
+        // //setup listener
+        // flightItem.addEventListener("click", function(e){setView(flight.latitude, flight.longitude)});
         document.getElementById("flights_list").appendChild(flightItem);
-        //setup listener
-        flightItem.addEventListener("click", function(){setView(latitude, longitude)});
-    }
-})
+        //}
+    });
+
+        document.querySelectorAll('.flight-item').forEach(item => {
+            const matchedFlight = flights.find(flight => flight.icao24address === item.id);
+            if (!!matchedFlight)
+            {
+                item.addEventListener('click', event => {
+                    setView(matchedFlight.latitude, matchedFlight.longitude);
+                })
+            }
+          });
+
+}
+)
 .catch((err) => console.log(err));
 
 function setView(latitude, longitude)
 {
-    map.setView([latitude,longitude], 8);
+    map.setView([latitude,longitude], 10);
 }
