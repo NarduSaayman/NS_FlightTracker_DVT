6d2142c4586c8fb4793973eb378a47dc1d060785
NarduSaayman <nsaayman@jhb.dvt.co.za>
2022-02-24 01:36:26 +0200

    style: :lipstick: Slight Styling Tweaks, added maping to interfaces

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import "./assets/styles/index.scss";
+import "../assets/styles/index.scss";
+import { Flight } from "./model/Flight";
+import { State } from "./model/State";
 
 // Leaflet map setup
 const map = L.map(`map`).setView([0, 0], 3);
 
 L.tileLayer(
-  `https://{s}.tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=4d525ab52e854ce982f0e8a5b2315989`,
+  `https://{s}.tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token=oU2G7Jcu16TVkU41lY2b9OYNvr12IhqiK1SPVkM349tunMkQkA3eSR1CCUPiyEIn`,
   {
-    attribution: `&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`,
+    attribution: `&copy; <a href="https://www.jawg.io/">JAWG</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`,
     maxZoom: 22,
   }
 ).addTo(map);
@@ -27,8 +29,8 @@ L.tileLayer(
 
 fetch(`https://opensky-network.org/api/states/all`)
   .then((response) => response?.json())
-  .then((data) => {
-    let flights = [];
+  .then((data: State) => {
+    const flights: Flight[] = [];
     const states = data?.states
       ?.filter(
         (state) =>
@@ -40,7 +42,7 @@ fetch(`https://opensky-network.org/api/states/all`)
       )
       .slice(0, 200);
 
-    states.forEach((state) => {
+    states?.map((state) => {
       const flight = {
         icao24address: state[0],
         callsign: state[1],
@@ -49,7 +51,7 @@ fetch(`https://opensky-network.org/api/states/all`)
         latitude: state[6],
         on_ground: state[8],
         true_track: state[10],
-      };
+      } as Flight
 
       // push onto flights array
       flights.push(flight);
@@ -59,8 +61,14 @@ fetch(`https://opensky-network.org/api/states/all`)
         icon: L.divIcon({
           className: `leaflet-plane-marker`,
           html: L.Util.template(
-            `<svg style="filter: invert(57%) sepia(11%) saturate(3678%) hue-rotate(189deg) brightness(98%) contrast(98%); -webkit-transform: rotate(${flight.true_track}deg); -moz-transform:rotate(${flight.true_track}deg);" version="1.0" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 97.239 100" enable-background="new 0 0 97.239 100" xml:space="preserve"><path d="M42.732,5.521c0.01-7.396,11.138-7.396,11.151,0.207v30.956l43.355,26.108v11.412L54.091,60.032v23.137l9.953,7.814V100l-15.373-4.741L33.248,100v-9.017l9.9-7.814V60.032L0,74.205V62.792l42.732-26.108V5.521L42.732,5.521z"></path></svg>`
-          ),
+            `<svg style="filter: invert(100%) sepia(0%) saturate(7478%) hue-rotate(227deg) brightness(109%) contrast(100%); 
+              stroke: #e6e5e5; 
+              stroke-width: 5px; 
+              -webkit-transform: rotate({Heading}deg); 
+              -moz-transform:rotate({Heading}deg);
+              " version="1.0" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 97.239 100" enable-background="new 0 0 97.239 100" xml:space="preserve"><path d="M42.732,5.521c0.01-7.396,11.138-7.396,11.151,0.207v30.956l43.355,26.108v11.412L54.091,60.032v23.137l9.953,7.814V100l-15.373-4.741L33.248,100v-9.017l9.9-7.814V60.032L0,74.205V62.792l42.732-26.108V5.521L42.732,5.521z"></path>
+              </svg>`
+          ,{Heading: flight.true_track}),
           iconAnchor: [12, 32],
           iconSize: [25, 30],
           popupAnchor: [0, -28],
@@ -74,20 +82,18 @@ fetch(`https://opensky-network.org/api/states/all`)
       // Add flight item to flights list
       const flightItem = document.createElement(`div`);
       flightItem.className = 
-      `
-      flight-item bg-slate-600 p-4 mb-4 mt-6
-      hover:cursor-pointer hover:bg-slate-500
+      `flight-item bg-jawgdark-500 p-4 mb-4 mt-6
+      hover:cursor-pointer hover:bg-jawgdark-400 hover:shadow-white-glow
       lg:z-10 lg:transition-all lg:ease-in-out lg:delay-50 lg:duration-200
       lg:hover:translate-x-1 lg:hover:w-[405px]
       border-solid border-1 rounded-md
-      drop-shadow-lg
-      overflow-visible
-      `;
+      drop-shadow-lg`;
+
       flightItem.id = flight.icao24address;
       flightItem.innerHTML = `
-      <div class="flight-item-header rounded-md bg-slate-500 p-1 text-center -mt-7 w-[50%] drop-shadow-md"><strong>Flight</strong> ${flight.callsign}</div>
-      <div class="pt-2"><strong>Origin</strong> - ${flight.origin_country}</div>`;
-      document.getElementById(`flights_list`).appendChild(flightItem);
+      <div class="flight-item-header hover:shadow-none shadow-white-glow rounded-md bg-jawgdark-50 p-1 text-center -mt-7 w-[50%] drop-shadow-md"><strong>Flight</strong> ${flight.callsign}</div>
+      <div class="pt-2 text-jawgdark-50"><strong>Origin</strong> - ${flight.origin_country}</div>`;
+      document.getElementById(`flights_list`)!.appendChild(flightItem);
     });
 
     // Setup eventListeners
@@ -104,6 +110,6 @@ fetch(`https://opensky-network.org/api/states/all`)
   })
   .catch((err) => console.error(err));
 
-function setMapView(latitude, longitude) {
+function setMapView(latitude: number, longitude: number) {
   map.flyTo([latitude, longitude], 8);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: [`./*.{html,js}`],
+  content: [`./*.{html,js,ts}`,`./src/*.{html,js,ts}`],
   theme: {
     extend: {
       height: {
@@ -14,6 +14,24 @@ module.exports = {
       transitionProperty: {
         'width': `width`
       },
+      colors: {
+        "jawgdark": {
+          "continent": `#343332`,
+          "50": `#E8E9E9`,
+          "100": `#D0D2D2`,
+          "200": `#A1A5A5`,
+          "300": `#737878`,
+          "400": `#464949`,
+          "500": `#191A1A`,
+          "600": `#141515`,
+          "700": `#0F1010`,
+          "800": `#0A0A0A`,
+          "900": `#050505`
+        }
+      },
+      boxShadow: {
+        'white-glow' : `0 0 10px 0px #fff`,
+      }
     },
   },
   plugins: [],
